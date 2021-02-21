2c41b00a3a389120bc5a6b1ffb4e53d0d546acc6
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-02-21 21:12:52 +0000

    redesigned pricing section

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -6,65 +6,59 @@ class Pricing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPrice: "Please select...",
       calculatedPrice: "...",
     };
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
+  handleChange(e) {
     this.setState({
-      calculatedPrice: priceCalculator(this.state.selectedPrice),
+      calculatedPrice: priceCalculator(e.target.value),
     });
   }
 
   render() {
     return (
-      <div id="section-pricing" className="pricing-section">
-        <h3>Price calculator...</h3>
-        <div className="form-wrapper">
-          <form id="price-calculator-form">
-            <label>Number of pictures</label>
-            <select
-              id="num-of-pics"
-              onChange={(e) => this.setState({ selectedPrice: e.target.value })}
-            >
-              <option>Please select...</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-              <option>9</option>
-              <option>10</option>
-              <option>11</option>
-              <option>12</option>
-              <option>13</option>
-              <option>14</option>
-              <option>15</option>
-              <option>16</option>
-              <option>17</option>
-              <option>18</option>
-              <option>19</option>
-              <option>20</option>
-            </select>
-            <button
-              id="price-calc-button"
-              type="submit"
-              onClick={(e) => this.handleSubmit(e)}
-            >
-              Calculate
-            </button>
-          </form>
-        </div>
-        <h1 id="calculated-price" className="price">
-          {parseFloat(this.state.calculatedPrice).toFixed(2) == "NaN"
-            ? this.state.calculatedPrice
-            : "£ " +
-              parseFloat(this.state.calculatedPrice).toFixed(2).toString()}
+      <div id="section-pricing" className="section-pricing">
+        <h1>Price Calculator</h1>
+        <h2>Receive an instant quote</h2>
+        <p>
+          How many rooms do you need photographed?{" "}
+          <div className="rooms-help">
+            <p>
+              A "room" is defined as any area (eg. kitchen, bedroom, garden,
+              garage, etc...) in your property that requires more than 2
+              photographs.
+            </p>
+            <i className="far fa-question-circle"></i>
+          </div>
+        </p>
+        <select id="num-of-pics" onChange={(e) => this.handleChange(e)}>
+          <option>Rooms...</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+          <option>13</option>
+          <option>14</option>
+          <option>15</option>
+          <option>16</option>
+          <option>17</option>
+          <option>18</option>
+          <option>19</option>
+          <option>20</option>
+        </select>
+        <h1 className="price">
+          {this.state.calculatedPrice == "..."
+            ? "..."
+            : "£ " + this.state.calculatedPrice}
         </h1>
         <p>
-          <span>Book now</span> with the form below!
+          Like what you see? <span>Book now</span>
         </p>
       </div>
     );
