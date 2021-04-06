e02469eaaa329f586cc14998f989fb11885fe73d
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-04-06 12:23:16 +0100

    Added BlogArticle styling and functionality

--- client/src/components/Blog/BlogArticle/BlogArticle.jsx
+++ client/src/components/Blog/BlogArticle/BlogArticle.jsx
@@ -2,16 +2,40 @@ import React, { Component } from "react";
 import { useParams } from "react-router-dom";
 
 import getBlogInfo from "../../utilities/getBlogInfo";
+import cardImage from "../../../images/IMG_0307-min.jpg";
+
+import "./blogArticle.css";
+
+const renderBody = (body) => {
+  return body.map(({ header, content }) => {
+    return (
+      <React.Fragment>
+        <h2>{header}</h2>
+        <p>{content}</p>
+      </React.Fragment>
+    );
+  });
+};
 
 const BlogArticle = () => {
   let { articleId } = useParams();
-
   const { title, subtitle, intro, body } = getBlogInfo(articleId);
 
+  console.log(body);
+
   return (
-    <h1>
-      {title}: {subtitle}
-    </h1>
+    <div className="section-article">
+      <div className="header-wrapper">
+        <img src={cardImage} alt="Article" />
+        <h1>
+          <span>{title}:</span> {subtitle}
+        </h1>
+      </div>
+      <div className="content-wrapper">
+        <p>{intro}</p>
+        {renderBody(body)}
+      </div>
+    </div>
   );
 };
 
--- client/src/components/utilities/getBlogInfo.js
+++ client/src/components/utilities/getBlogInfo.js
@@ -1,11 +1,17 @@
 // @args Path to directory, filename
 export default (filename) => {
-  const { title, subtitle, intro } = require(`../Blog/articleData/${filename}`);
+  const {
+    title,
+    subtitle,
+    intro,
+    body,
+  } = require(`../Blog/articleData/${filename}`);
 
   return {
     filename,
     title,
     subtitle,
     intro,
+    body,
   };
 };
