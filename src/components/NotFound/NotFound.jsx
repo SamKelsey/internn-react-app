b74e4d83fc338b21e2a7da76c8d4b692845a0dd8
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-04-27 20:22:08 +0100

    Added 404 page.

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeader from "../utilities/PageHeader/PageHeader";
+
+const NotFound = () => {
+  return (
+    <React.Fragment>
+      <PageHeader title="404 - Oops! This page no longer exists." />
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
