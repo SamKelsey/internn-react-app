8aad5e56740c3fd77dc575fd84f87d2e3131418d
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-07-05 21:03:28 +0100

    test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ class Footer extends Component {
         </div>
         <div className="terms-link-wrapper">
           <Link to="/terms-and-conditions" className="terms-link">
-            Terms & Conditions and Privacy Policy test2.
+            Terms & Conditions and Privacy Policy.
           </Link>
         </div>
       </div>
