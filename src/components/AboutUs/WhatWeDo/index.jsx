dc5004ba873bc5436ea62e6f357336102257a990
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-08 14:26:59 +0100

    added new font weight and some styling.

--- src/components/AboutUs/WhatWeDo/index.jsx
+++ src/components/AboutUs/WhatWeDo/index.jsx
@@ -6,17 +6,17 @@ const WhatWeDo = () => {
     <div className="what-we-do">
       <h2>What we do</h2>
       <p>
-        Here at internn, we offer stunning property photography, but our pricing
-        is what really sets us apart! There's a lot of property photography
-        companies out there, so what makes us different? Well, all of our
-        photographers are students! We hire top performing students with a real
-        passion for photography and offer them the opportunity to gain
-        experience, build their portfolios and also earn from a part-time job,
-        photographing your properties! Therefore, through using internn you're
-        helping students gain experience in their dream careers. Alongside
-        supporting upcoming talent, we want to make life easy for property
-        owners. As such, using internn is simple, just follow the steps below.
-        We can't wait to hear from you!
+        Here at internn, we offer <span>stunning property photography,</span>{" "}
+        but our pricing is what really sets us apart! There's a lot of property
+        photography companies out there, so what makes us different? Well,{" "}
+        <span>all of our photographers are students!</span> We hire top
+        performing students with a real passion for photography and offer them
+        the opportunity to gain experience, build their portfolios and also earn
+        from a part-time job, photographing your properties! Therefore, through
+        using internn you're helping students gain experience in their dream
+        careers. Alongside supporting upcoming talent, we want to make life easy
+        for property owners. As such, using internn is simple, just follow the
+        steps below. We can't wait to hear from you!
       </p>
     </div>
   );
