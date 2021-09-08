4798e10197b63b78032922ac99ad1ff1a73aa4d0
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-08 17:53:15 +0100

    restyled WhatWeDo section.

--- src/components/AboutUs/WhatWeDo/index.jsx
+++ src/components/AboutUs/WhatWeDo/index.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import FadeInSection from "../../utils/FadeInSection";
 import "./whatWeDo.scss";
 
 const WhatWeDo = () => {
   return (
     <div className="what-we-do">
       <h2>What we do</h2>
-      <p>
-        Here at internn, we offer <span>stunning property photography,</span>{" "}
-        but our pricing is what really sets us apart! There's a lot of property
-        photography companies out there, so what makes us different? Well,{" "}
-        <span>all of our photographers are students!</span> We hire top
-        performing students with a real passion for photography and offer them
-        the opportunity to gain experience, build their portfolios and also earn
-        from a part-time job, photographing your properties! Therefore, through
-        using internn you're helping students gain experience in their dream
-        careers. Alongside supporting upcoming talent, we want to make life easy
-        for property owners. As such, using internn is simple, just follow the
-        steps below. We can't wait to hear from you!
-      </p>
+      <FadeInSection>
+        <div className="text-wrapper">
+          <i class="fas fa-quote-left left"></i>
+          <p>
+            Here at internn, we offer stunning property photography, but our
+            pricing is what really sets us apart! There's a lot of property
+            photography companies out there, so what makes us different? Well,
+            all of our photographers are students! We hire top performing
+            students with a real passion for photography and offer them the
+            opportunity to gain experience, build their portfolios and also earn
+            from a part-time job, photographing your properties! Therefore,
+            through using internn you're helping students gain experience in
+            their dream careers. Alongside supporting upcoming talent, we want
+            to make life easy for property owners. As such, using internn is
+            simple, just follow the steps below. We can't wait to hear from you!
+          </p>
+          <i class="fas fa-quote-right right"></i>
+        </div>
+      </FadeInSection>
     </div>
   );
 };
