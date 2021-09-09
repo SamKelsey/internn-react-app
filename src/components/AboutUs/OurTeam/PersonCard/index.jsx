2b1ecdd0505125291ea585483b5ac2738cc33919
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-09 21:50:17 +0100

    added our team card component

--- src/components/AboutUs/OurTeam/PersonCard/index.jsx
+++ src/components/AboutUs/OurTeam/PersonCard/index.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./personCard.scss";
+import config from "../../../../config";
 
 const PersonCard = ({ name, role, bio, instagram, facebook, image }) => {
   return (
-    <div>
-      <div>{name}</div>
-      <div>{role}</div>
-      <div>{bio}</div>
-      <div>{instagram}</div>
-      <div>{facebook}</div>
-      <div>{image}</div>
+    <div className="person-card">
+      <div className="person-card-text">
+        <div className="headers">
+          <h2>{name}</h2>
+          <h3>{role}</h3>
+        </div>
+        <p>{bio}</p>
+        <Link className="person-card-button">View my work</Link>
+      </div>
+      <img
+        src={`${config["s3-images-url"]}/IMG_0220.JPG`}
+        alt="profile picture"
+      />
     </div>
   );
 };
