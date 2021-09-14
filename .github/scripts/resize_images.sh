5a1ef123e454f6b2bd6a3784ba944505a9e32dec
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-14 20:49:07 +0100

    filtered out already formatted images

--- .github/scripts/resize_images.sh
+++ .github/scripts/resize_images.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
+# This script is for creating images of the correct size and name.
 cd images
-for FILE in *;
-do 
-if [[ $FILE == *.JPG ]] || [[ $FILE == *.jpg ]]; then
-    width=$(identify -format '%w' $FILE);
-    filename="${FILE%.*}"
+for FILE in *; do 
+    if [[ $FILE == *.JPG ]] || [[ $FILE == *.jpg ]]; then
+        width=$(identify -format '%w' $FILE);
+        filename="${FILE%.*}"
 
-    if [[  ]]
-    
-    if [[ $width -gt 2000 ]]; then
-        echo "large image here";
-    elif [[ $width -gt 800 ]]; then
-        echo "small image: ${filename}";
-        convert $FILE -resize 800 "${filename}-sm.jpg";
+        if [[ ${filename: -4} ==  "orig" ]] || [[ ${filename: -2} ==  "sm" ]] || [[ ${filename: -2} ==  "md" ]] || [[ ${filename: -2} ==  "lg" ]]; then
+            continue;
+        elif [[ $width -gt 2000 ]]; then
+            echo "medium image here";
+            convert $FILE -resize 2000 "${filename}-md.jpg";
+        elif [[ $width -gt 800 ]]; then
+            echo "small image: ${filename}";
+            convert $FILE -resize 800 "${filename}-sm.jpg";
+        fi
+
+        mv $FILE "${filename}-orig.jpg";
     fi
-
-    
-
-fi
 done
