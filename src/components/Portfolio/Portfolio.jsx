9e8cab0617d370e09454b138789d77dbeb420448
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-22 11:16:35 +0100

    refactored portfolio page

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from "react";
+import "./portfolio.scss";
+import { Helmet } from "react-helmet";
+import TopTile from "../utils/TopTile";
+import PhotoGallery from "../utils/PhotoGallery";
+
+const portfolioImages = [
+  "IMG_1176_portrait",
+  "IMG_1472",
+  "IMG_1472",
+  "IMG_1176_portrait",
+  "IMG_1472",
+];
+
+const Portfolio = () => {
+  return (
+    <div className="section-portfolio">
+      <Helmet>
+        <title>Portfolio. | internn</title>
+        <meta
+          name="description"
+          content="Seeing is believing. View our portfolio of property photographs taken by our incredible student photographers. Stunning, low-cost photographs."
+        />
+      </Helmet>
+      <TopTile
+        image="IMG_2471-HDR-lg.jpg"
+        title="Portfolio"
+        subtitle='"Our quality speaks for itself"'
+      />
+      <PhotoGallery images={portfolioImages} />
+    </div>
+  );
+};
+
+export default Portfolio;
