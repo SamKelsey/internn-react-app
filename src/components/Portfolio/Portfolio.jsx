1b1e64507855fd39c667f08860e01b97b135a308
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-09-28 20:16:22 +0100

    removed some warnings

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import "./portfolio.scss";
 import { Helmet } from "react-helmet";
 import TopTile from "../utils/TopTile";
--- src/components/utils/HeaderLogo/index.jsx
+++ src/components/utils/HeaderLogo/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import logo from "../../../images/original_design_with_slogan.png";
