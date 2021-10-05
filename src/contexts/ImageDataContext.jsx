53a8b6b8ce7387d0f382af61206880eb4d7225d6
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-10-05 18:32:13 +0100

    removed incorrect import

--- src/contexts/ImageDataContext.jsx
+++ src/contexts/ImageDataContext.jsx
@@ -1,5 +1,4 @@
-import React, { createContext, useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { createContext, useState, useEffect } from "react";
 
 const ImageDataContext = createContext();
 
